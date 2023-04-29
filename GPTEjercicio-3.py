8a140368889f1f40c3169e2a6bf7e4d25b90cd0e
Jorge Martínez <117450327+FooJorge@users.noreply.github.com>
2023-04-29 17:58:25 +0200

    Add files via upload

---
+++ GPTEjercicio-3.py
@@ -0,0 +1,6 @@
+# Ejercicio: Escribe un programa que solicite al usuario una palabra y luego muestre
+# la longitud de la palabra.
+
+a=input('Dime una palabra: ')
+b=len(a)
+print('El número de letras de', a, 'son:', b)
