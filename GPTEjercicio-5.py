065d9c871d0ad1fbc253e25cf5f0b9aeea40806d
Jorge Martínez <117450327+FooJorge@users.noreply.github.com>
2023-05-01 11:34:43 +0200

    Add files via upload

---
+++ GPTEjercicio-5.py
@@ -0,0 +1,7 @@
+# Ejercicio: Escribe un programa que solicite al usuario una frase y luego muestre la
+# cantidad de palabras que contiene.
+
+frase=input('Escribe una frase: ')
+espacios=frase.count(' ')
+palabras=int(espacios)+1 #Una frase contiene una palabra más que el número de espacios que tiene
+print("La frase:", frase, "contiene", palabras, 'palabras')
