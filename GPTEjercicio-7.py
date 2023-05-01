afda59195daba559432c616249cdb603670d4377
Jorge Martínez <117450327+FooJorge@users.noreply.github.com>
2023-05-01 12:52:19 +0200

    Update GPTEjercicio-7.py
    Corrección de ChatGPT

--- GPTEjercicio-7.py
+++ GPTEjercicio-7.py
@@ -1,8 +1,16 @@
 #Ejercicio: Escribe un programa que solicite al usuario una frase y 
 #luego muestre la cantidad de veces que aparece cada palabra en la frase.
 
-frase=input("Introduce una frase: ")
-palabras=frase.split() #type list
-numero_palabras=len(palabras)
-for i in range(0,numero_palabras):
-    print(palabras[i],"=",palabras.count(palabras[i]))
+frase = input("Introduce una frase: ")
+palabras = frase.split()
+frecuencia_palabras = {}
+
+for palabra in palabras:
+    if palabra in frecuencia_palabras:
+        frecuencia_palabras[palabra] += 1
+    else:
+        frecuencia_palabras[palabra] = 1
+
+print("Frecuencia de palabras:")
+for palabra, frecuencia in frecuencia_palabras.items():
+    print(palabra, "-", frecuencia)
