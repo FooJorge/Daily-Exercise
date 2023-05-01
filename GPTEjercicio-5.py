4c7fed663c2ac425ec01fff2f3b2fc064e494d49
Jorge Martínez <117450327+FooJorge@users.noreply.github.com>
2023-05-01 11:43:19 +0200

    Update GPTEjercicio-5.py
    Solución sugerida por ChatGPT donde se introduce la función split, la cual divide la frase en palabras y luego con la función len se cuenta el número de palabras

--- GPTEjercicio-5.py
+++ GPTEjercicio-5.py
@@ -1,7 +1,7 @@
 # Ejercicio: Escribe un programa que solicite al usuario una frase y luego muestre la
 # cantidad de palabras que contiene.
 
-frase=input('Escribe una frase: ')
-espacios=frase.count(' ')
-palabras=espacios+1 #Una frase contiene una palabra más que el número de espacios que tiene
-print("La frase:", frase, "contiene", palabras, 'palabras')
+frase = input("Introduce una frase: ")
+cantidad_palabras = len(frase.split()) #la función split divide la frase en palabras y la función len cuenta el número de palabras 
+
+print("La frase:", frase, "tiene", cantidad_palabras, "palabras.")
