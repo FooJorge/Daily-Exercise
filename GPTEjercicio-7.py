5a24f8d63fc6d37c92a30a6b1bd63d2bbf6e3259
Jorge Martínez <117450327+FooJorge@users.noreply.github.com>
2023-05-02 10:20:42 +0200

    Update GPTEjercicio-7.py
    Añado comentarios para entender mejor la solución

--- GPTEjercicio-7.py
+++ GPTEjercicio-7.py
@@ -2,15 +2,15 @@
 #luego muestre la cantidad de veces que aparece cada palabra en la frase.
 
 frase = input("Introduce una frase: ")
-palabras = frase.split()
-frecuencia_palabras = {}
+palabras = frase.split() #separo la frase en una lista de palabras
+frecuencia_palabras = {} #creo un diccionario vacío. Estructura de un diccionario {'clave1':valor1, 'clave2': valor2,...}
 
-for palabra in palabras:
+for palabra in palabras: #si la palabra está en el diccionario sumo uno, si no, añado solo la palabra
     if palabra in frecuencia_palabras:
         frecuencia_palabras[palabra] += 1
     else:
         frecuencia_palabras[palabra] = 1
 
 print("Frecuencia de palabras:")
-for palabra, frecuencia in frecuencia_palabras.items():
+for palabra, frecuencia in frecuencia_palabras.items(): #item() de vuelve una lista de tuplas formada por los pares (clave,valor)
     print(palabra, "-", frecuencia)
